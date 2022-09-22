4180ae7fe8bb69cbcfca909288b8c84b9dc030c4
mhh <1147905003@qq.com>
2022-09-23 04:29:34 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        phone: "",
+        password: "",
+      },
+      rules: [{}],
+    };
+  },
+};
+</script>
+<style scoped>
+.app {
+  width: 900px;
+}
+</style>
